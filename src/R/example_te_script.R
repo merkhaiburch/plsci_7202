212ab684455112dd1a01b199ccfb19a84487668c
Merritt Burch <mbb262@cornell.edu>
2020-10-02 10:02:54 -0400

    annotating scripts

--- src/R/example_te_script.R
+++ src/R/example_te_script.R
@@ -3,7 +3,7 @@
 # Contact... mbb262@cornell.edu
 # Date...... 2020-10-01
 #
-# Description 
+# Description
 #   - Exploring maize TE annotations from Stitzer 2019
 # ---------------------------------------------------------------
 
@@ -31,7 +31,7 @@ closest_te1 <- te1[closest_gene, nomatch = 0]
 # Analysis
 #--------------
 
-# Look at relationship between B73 mature leaf expression and 
+# Look at relationship between B73 mature leaf expression and
 #     all TEs genome wide + their distance away from genes
 model_dist_sup <- lm(log10(gene_Mature_Leaf_8+1) ~ closest+ sup, data = te1)
 summary(model_dist_sup)
@@ -65,15 +65,15 @@ temp4$distance <- rep(20000, nrow(temp4))
 temp5$distance <- rep(10000, nrow(temp5))
 temp6$distance <- rep(3000, nrow(temp6))
 
-# Rbind all together
+# Rbind all together and filter
 together <- rbind(temp0, temp1, temp2, temp3, temp4, temp5, temp6) %>% filter(count >20)
 
 # Plot across all superfamilies
-ggplot(together, aes(x=as.factor(distance), y=log10(count))) + 
+ggplot(together, aes(x=as.factor(distance), y=log10(count))) +
   geom_boxplot() +
   xlab("Distance away from closest gene")
 
 # Color by superfamily
-ggplot(together, aes(x=as.factor(distance), y=log10(count), fill = sup)) + 
+ggplot(together, aes(x=as.factor(distance), y=log10(count), fill = sup)) +
   geom_boxplot() +
   xlab("Distance away from closest gene")
